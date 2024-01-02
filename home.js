d03cb40ab041719cce76d2f5d3e6f4e0fbbed31d
MkAsif <asifk@fermion.in>
2024-01-02 11:29:50 +0530

    html formatting

--- home.js
+++ home.js
@@ -1,35 +1,8 @@
-$(".slide").slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 6,
-  slidesToScroll: 6,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
-});
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
